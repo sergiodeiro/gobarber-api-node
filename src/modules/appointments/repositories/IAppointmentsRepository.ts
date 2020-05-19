c2b7ccefdd0d6f3c0d3ac416721f406c4e8ca3dc
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-05-19 18:27:18 -0300

    Terminando o módulo de perfil de usuario e iniciando o módulo de agendamentos

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,15 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthProviderDTO from '../dtos/IFindAllInMonthProviderDTO';
+import IFindAllInDayProviderDTO from '../dtos/IFindAllInDayProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(data: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayProviderDTO,
+  ): Promise<Appointment[]>;
 }
